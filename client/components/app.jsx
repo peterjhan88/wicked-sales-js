3f372374dd8a8a355bf1f64b32daa92e60cc84d6
Peter Han <peterjhan88@gmail.com>
2020-01-14 18:00:06 -0800

    Completed add-to-cart-front-end | Peter Han

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,9 +10,11 @@ export default class App extends React.Component {
       view: {
         name: 'catalog',
         params: {}
-      }
+      },
+      cart: []
     };
     this.setView = this.setView.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
   setView(name, params) {
@@ -24,13 +26,61 @@ export default class App extends React.Component {
     });
   }
 
+  getCartItems() {
+    fetch('/api/cart')
+      .then(result => result.json())
+      .then(jsonData => {
+        this.setState({ cart: jsonData });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  addToCart(product) {
+    const productToAdd = {
+      productId: product.productId
+    };
+    const headersToAdd = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(productToAdd)
+    };
+    fetch('/api/cart', headersToAdd)
+      .then(response => response.json())
+      .then(jsonData => {
+        this.setState(previousState => {
+          var newCart = previousState.cart;
+          newCart.push(jsonData);
+          return { cart: newCart };
+        });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  componentDidMount() {
+    this.getCartItems();
+  }
+
   render() {
     return (
       <>
-        <Header />
-        {this.state.view.name === 'catalog'
-          ? <ProductList setView={this.setView} />
-          : <ProductDetails params={this.state.view.params} setView={this.setView}/>}
+        <Header cartItemCount={this.state.cart.length}/>
+        {
+          this.state.view.name === 'catalog'
+            ? <ProductList
+              setView={this.setView}
+            />
+            : <ProductDetails
+              params={this.state.view.params}
+              setView={this.setView}
+              addToCart={this.addToCart}
+            />
+        }
       </>
     );
   }
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,8 +4,9 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className='bg-dark d-flex header-height align-items-center'>
-        <div className='header-logo'>$</div>
-        <div className='header-name'>Wicked Sales</div>
+        <div className='header-logo'><i className="fas fa-dollar-sign"></i></div>
+        <div className='header-name mr-auto'>Wicked Sales</div>
+        <div className='mr-5 shopping-cart'><span>{this.props.cartItemCount} items</span> <i className='fas fa-shopping-cart'></i></div>
       </header>
     );
   }
